e7dbfb5e97883cc2e91bc91194ba90db49616f25
the-last-php-bender <nwinyinyadavid123@gmail.com>
2024-06-29 22:07:11 +0100

    FIxed all pending bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import sequelize from './api/config/database.js';
 import urlRoutes from './api/routes/routes.js';
 import swaggerUi from 'swagger-ui-express';
@@ -8,14 +9,17 @@ import swaggerSpecs from './swaggerOptions.js';
 const app = express();
 const port = 3000;
 
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-app.get('/',function(req,res){
-  res.send("Yes the server is live🚗🚗🚗🚗🚗🚗🚗🚗🚗")
-})
+
+app.get('/', function(req, res) {
+  res.send("Yes the server is live🚗🚗🚗🚗🚗🚗🚗🚗🚗");
+});
+
 app.use('/api', urlRoutes);
-
-
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
